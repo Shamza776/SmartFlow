f51fda8d75fee1d58506b7d69ae574818cd73e5f
Sharleen Jamoza <sharleenjamoza@gmail.com>
2025-02-09 13:31:21 +0300

    tried to redeploy the sungraph

--- backend/subgraph/smartflowsubgraph/src/mapping.ts
+++ backend/subgraph/smartflowsubgraph/src/mapping.ts
@@ -1,9 +1,11 @@
-import { Swap as SwapEvent } from "../generated/UniswapV3Pool/UniswapV3Pool";
-import { Swap } from "../generated/schema";
+import { Swap } from "../generated/UniswapV3Pool/UniswapV3Pool";
+import { Swap as SwapEvent } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
+import { PoolCreated as PoolCreatedEvent } from "../generated/UniswapV3Factory/UniswapV3Factory";
+import { PoolCreated } from "../generated/schema";
 
-export function handleSwap(event: SwapEvent): void {
-  let swap = new Swap(event.transaction.hash.toHexString());
+export function handleSwap(event: Swap): void {
+  let swap = new SwapEvent(event.transaction.hash.toHexString());
   swap.sender = event.params.sender;
   swap.recipient = event.params.recipient;
   swap.amount0 = event.params.amount0;
@@ -19,4 +21,19 @@ export function handleSwap(event: SwapEvent): void {
   swap.isWhale = swap.amount0.gt(whaleThreshold) || swap.amount1.gt(whaleThreshold);
 
   swap.save();
+}
+
+export function handlePoolCreated(event: PoolCreatedEvent): void {
+  let pool = new PoolCreated(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+
+  pool.id = event.params.pool.toHex();
+  pool.token0 = event.params.token0;
+  pool.token1 = event.params.token1;
+  pool.tickSpacing = BigInt.fromI32(event.params.tickSpacing);
+  pool.pool = event.params.pool;
+  pool.transactionHash = event.transaction.hash;
+  pool.blockNumber = event.block.number;
+  pool.blockTimestamp = event.block.timestamp;
+
+  pool.save();
 }
--- backend/subgraph/smartflowsubgraph/src/uniswap-v-3-factory.ts
+++ backend/subgraph/smartflowsubgraph/src/uniswap-v-3-factory.ts
@@ -1,21 +1,21 @@
-// import {
-//   PoolCreated as PoolCreatedEvent
-// } from "../generated/UniswapV3Factory/UniswapV3Factory"
-// import {
-//   PoolCreated
-// } from "../generated/schema"
+import {
+  PoolCreated as PoolCreatedEvent
+} from "../generated/UniswapV3Factory/UniswapV3Factory"
+import {
+  PoolCreated
+} from "../generated/schema"
 
-// export function handlePoolCreated(event: PoolCreatedEvent): void {
-//   let entity = new PoolCreated(
-//     event.transaction.hash.toHex().concat("-").concat(event.logIndex.toString())
-//    )
-//   entity.token0 = event.params.token0
-//   entity.token1 = event.params.token1
-//   entity.pool = event.params.pool
+export function handlePoolCreated(event: PoolCreatedEvent): void {
+  let entity = new PoolCreated(
+    event.transaction.hash.toHex().concat("-").concat(event.logIndex.toString())
+   )
+  entity.token0 = event.params.token0
+  entity.token1 = event.params.token1
+  entity.pool = event.params.pool
 
-//   entity.blockNumber = event.block.number
-//   entity.blockTimestamp = event.block.timestamp
-//   entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
 
-//   entity.save()
-// }
+  entity.save()
+}
